f19375046056147e893890a6ea18059308a375e0
4727yesuraju <94819056+4727yesuraju@users.noreply.github.com>
2024-07-26 13:35:18 +0530

    vite file is changed

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        secure: false,
-      },
-    },
-  },
-
-  plugins: [react()],
+	plugins: [react()],
+	server: {
+		proxy: {
+			"/api": {
+				target: "http://localhost:5000",
+			},
+		},
+	},
 });
